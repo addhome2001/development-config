e8dfaee0d6720211342bf25e6ded8c7353f410de
Seven <seven.chi@shopback.com>
2021-02-19 14:46:37 +0800

    Add alias for git rebase

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
+git config --global alias.rei 'rebase -i'
 
 cp ./.zshrc ~/.zshrc
 cp ./vimrc ~/.vim/vimrc
